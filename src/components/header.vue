6c3651fcb83106ac7a1697ede3b2df2ea3a9317c
CokeWithIce <jklkofjklkof@163.com>
2025-05-31 17:24:08 +0800

    header页面的修改

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,15 +1,29 @@
 <template>
 	<el-header class="header">
 		<el-row class="row">
-			<el-col :span="24" >
-				<img src="../assets/logo.png" height="30" />
-				<text>adfasdf</text>
+			<el-col :span="6">
+				<div class="container">
+					<img src="/public/logo.png" height="50" />
+					<span class="title">启航星文化传媒工作室</span>
+				</div>
 			</el-col>
+			<el-row :span="18">
+				<div class="menu">
+					<span>大型文演</span>
+					<span>公司纪录片</span>
+					<span>文艺设计</span>
+					<span>线下展览</span>
+					<span>联系我们</span>
+				</div>
+			</el-row>
 		</el-row>
 	</el-header>
 </template>
 
 <script setup>
+import { ElContainer } from 'element-plus';
+import { ref } from 'vue';
+const activeIndex=ref(1);
 </script>
 
 <style lang="scss" scoped>
@@ -21,14 +35,18 @@
 			width: 1200px;
 			margin: 0px auto;
 			height:30px;
-			text {
-				line-height: 30px;
-				height: 30px;
-				font-size: 30px;
-				font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-				padding-left:10px;
-			}
 		}
-
+		.menu{
+			display: flex;
+			justify-self: center;
+		}
+		.menu>span{
+			flex:1;
+		}
+	}
+	.container{
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 </style>
---
+++ src/components/swiper.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="wrapper">
+    <div>
+        
+    </div>
+</div>
+</template>
+<script setup>
+</script>
+<style lang="scss" scoped>
+</style>
